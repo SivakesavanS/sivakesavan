3aebbb2addb708f0742aae113650c17f88123276
siva <siva@siva>
2022-08-26 08:43:14 +0530

    samble program

---
+++ src/samble1/Samble.java
@@ -0,0 +1,16 @@
+package samble1;
+
+public class Samble {
+
+	public static void main(String[] args) {
+		int num =10;
+		System.out.println(num);
+		num=20;
+		System.out.println(num);
+		double num2=45.36657645;
+		System.out.print(num2);
+		// TODO Auto-generated method stub
+
+	}
+
+}
