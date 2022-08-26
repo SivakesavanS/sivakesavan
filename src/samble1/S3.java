98bb29f94008bb01750d8ed5d873c74354474059
siva <siva@siva>
2022-08-26 09:43:35 +0530

    Samble program3

---
+++ src/samble1/S3.java
@@ -0,0 +1,13 @@
+package samble1;
+
+public class S3 {
+	public static void main(String[] args) {
+		int n1=5 , n2=21;
+		int n3;
+		n3=n1;
+		n1=n2;
+		n2=n3;
+		System.out.println(n1);
+		System.out.println(n2);
+	}
+}
