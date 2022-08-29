4a7310c9f38e27dd68984b526bff5f98a418037f
siva <siva@siva>
2022-08-29 10:09:09 +0530

    spilit 3 digit

---
+++ src/samble1/Spilit3digit.java
@@ -0,0 +1,15 @@
+package samble1;
+
+public class Spilit3digit {
+	public static void main(String[] args) {
+		int num=157;
+		int a,b,c,d;
+		a=num/10;
+		b=num%10;
+		c=a/10;
+		d=a%10;
+		System.out.println(c);
+		System.out.println(d);
+		System.out.println(b);
+	}
+}
