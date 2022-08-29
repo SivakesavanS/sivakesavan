29821573998059bd035a228f51701a8ce1d0a388
siva <siva@siva>
2022-08-29 11:16:37 +0530

    Spilit 2 Digit

---
+++ src/samble1/Spilit2Digits.java
@@ -0,0 +1,13 @@
+package samble1;
+
+public class Spilit2Digits {
+	public static void main(String[] args) {
+		int num=35;
+		int a,b;
+		a=num/10;
+		b=num%10;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
