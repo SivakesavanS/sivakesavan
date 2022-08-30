51d39fb57807ef79d26e5bbc977a5b0d5f8abed5
siva <siva@siva>
2022-08-30 08:21:42 +0530

    Arithamatic process

---
+++ src/samble1/aritha1.java
@@ -0,0 +1,14 @@
+package samble1;
+
+public class aritha1 {
+	public static void main(String[]args) {
+		int a=10;
+		int b=5;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println(a);
+		System.out.print(b);
+	}
+
+}
