83b7b078737d724c76ac690e29899bbe03664dd8
siva <siva@siva>
2022-08-31 18:40:41 +0530

    Increment and Degrement

---
+++ src/samble1/Incrementdecrement.java
@@ -0,0 +1,11 @@
+package samble1;
+
+public class Incrementdecrement {
+	public static void main(String[]args) {
+		int x=5;
+		int a=--x;
+		System.out.println(a);
+		System.out.println(x);	
+	}
+
+}
