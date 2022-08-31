5512efe78f525df233629bf7ca4058dfcf78fa9d
siva <siva@siva>
2022-08-31 19:25:15 +0530

    Arrays Program

---
+++ src/practics/Arraysint.java
@@ -0,0 +1,19 @@
+package practics;
+import java.util.Arrays;
+
+public class Arraysint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+ int [] num=new int[5];
+ char name[]= {'a','s'};
+num[0]=64;
+num[4]=62;
+
+System.out.println(num[0]);
+System.out.println(name);
+
+
+	}
+
+}
