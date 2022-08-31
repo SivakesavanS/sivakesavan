4e0a4c4ed7780a5c0d1d1c45745501e0fe29e021
siva <siva@siva>
2022-08-31 18:37:47 +0530

    Decision Making Program

---
+++ src/practics/DecisionMakingProgram.java
@@ -0,0 +1,16 @@
+package practics;
+
+public class DecisionMakingProgram {
+	public static void main(String[]args) {
+		int a=25;
+		if(a>=18&&a<=55) {
+			System.out.println("ok");
+		}
+		else {
+			System.out.println("not ok");
+		}
+	}
+	
+	
+
+}
