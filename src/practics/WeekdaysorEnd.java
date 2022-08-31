51c4bd0d22be0dbb116bcc5c93f9d4ea59b0076d
siva <siva@siva>
2022-08-31 18:44:25 +0530

    Weekdays and Weekend Program

---
+++ src/practics/WeekdaysorEnd.java
@@ -0,0 +1,15 @@
+package practics;
+
+public class WeekdaysorEnd {
+	public static void main(String[]args) {
+	int a=6;
+	if(a<=5&&a>=1) {
+		System.out.println("weekdays");
+	}
+	else if(a==6||a==7) {
+		System.out.print("weekend");
+	}
+		
+	}
+
+}
