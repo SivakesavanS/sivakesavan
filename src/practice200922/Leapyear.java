ca4d3919c03ceb3db87ba19d8463c288717df620
siva <siva@siva>
2022-09-21 10:36:46 +0530

    Leap Year Program

---
+++ src/practice200922/Leapyear.java
@@ -0,0 +1,15 @@
+package practice200922;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+      int n=2002;
+      if(n%4==0) {
+    	  System.out.print("Leap Year");
+      }else {
+    	  System.out.print("Not Leap Year");  
+      }
+	}
+
+}
