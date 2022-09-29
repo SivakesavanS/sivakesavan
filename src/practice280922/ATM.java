1fa129dcc72f62773c9c45a1790f572ae90134d5
siva <kesavansiva515@gmail.com>
2022-09-29 14:25:49 +0530

    ATM Cash Distribution Program

---
+++ src/practice280922/ATM.java
@@ -0,0 +1,25 @@
+package practice280922;
+
+public class ATM {
+     
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+    int n=4300;
+    int s=0;
+    int a,b,c;
+    
+    a=n/500;
+    System.out.println(a);
+    a=n%500;
+   // System.out.println(a);
+    b=a/200;
+    System.out.println(b);
+    b=a%200;
+   // System.out.println(b);
+    c=b/100;
+    System.out.println(c);
+    c=b%100;
+  //  System.out.println(c);
+	}
+
+}
