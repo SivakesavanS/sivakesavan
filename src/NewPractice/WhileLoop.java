c76aef801815745e7f38db42b99b7facae84b4d6
siva <kesavansiva515@gmail.com>
2022-10-09 23:45:27 +0530

    New Idea From Muthu Sir To While And For Looping Programs

---
+++ src/NewPractice/WhileLoop.java
@@ -0,0 +1,35 @@
+package NewPractice;
+
+public class WhileLoop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//int num=1;   
+//while(num<=5) {
+//	System.out.println(num);  ----->1,2,3,4,5
+//	num++;
+//}
+//int num=5;
+//while(num>=1) {
+//	System.out.println(num);  ----->5,4,3,2,1
+//	num--;
+//}
+//int num=0;
+//while(num<=10) {
+//	System.out.println(num);  ---->0,2,4,6,8,10
+//	num=num+2;
+//}
+//int num=1;
+//while(num<=10) {
+//	System.out.println(num); ---->1,3,5,7,9
+//	num=num+2;
+//}
+int num=11;
+while(num<=100) {
+	System.out.println(num);
+	num=num+11;
+}
+		
+	}
+
+}
