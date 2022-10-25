b9d2680461fe63a5be70cc6f192df57560cbd6e9
siva <kesavansiva515@gmail.com>
2022-10-25 13:39:16 +0530

    Difference Between Equal Operator And Equal Method

---
+++ src/string/EquelOperatorAndMethod.java
@@ -0,0 +1,14 @@
+package string;
+
+public class EquelOperatorAndMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s=new String("Siva");
+		String s1=new String("Siva");
+		System.out.println(s==s1);
+		System.out.println(s.equals(s1));
+		
+	}
+
+}
